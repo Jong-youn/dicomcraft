1745c36e6bfc2c4450afca637eed6807e649addc
Jongyoun Lee <jayylee.dev@gmail.com>
2025-08-11 16:54:53 +0900

    init

---
+++ src/main/java/io/morningowl/dicomcraft/DicomcraftApplication.java
@@ -0,0 +1,13 @@
+package io.morningowl.dicomcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicomcraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicomcraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/morningowl/dicomcraft/DicomcraftApplicationTests.java
@@ -0,0 +1,13 @@
+package io.morningowl.dicomcraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DicomcraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
